a96c8eb7477f76b4d0aca95aa9e1867983c12812
Jayce <datzlatannn@gmail.com>
2025-01-23 09:59:01 +0000

    updated platform

--- tools/build_docker.sh
+++ tools/build_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DOCKER_PLATFORMS='linux/amd64,linux/arm64'
+#DOCKER_PLATFORMS='linux/amd64,linux/arm64'
+DOCKER_PLATFORMS='linux/amd64'
 registry=''     # e.g. 'Dakuchi/'
 
 print_usage() {
@@ -18,29 +19,29 @@ done
 if [ $DEPLOY == 1 ]
 then
 	docker run -it --rm --privileged tonistiigi/binfmt --install all
-	docker  create --use --name mybuilder
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/ --push
-	perl -i -pe's|.*FROM Dakuchi/|FROM '"${registry}"'|g' ../services/tools.descartes.teastore.*/Dockerfile
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/ --push
-	docker  build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/ --push
-	perl -i -pe's|.*FROM '"${registry}"'|FROM Dakuchi/|g' ../services/tools.descartes.teastore.*/Dockerfile
-	docker  rm mybuilder
+	docker buildx create --use --name mybuilder
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/ --push
+	perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'|g' ../services/tools.descartes.teastore.*/Dockerfile
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/ --push
+	docker buildx build --platform ${DOCKER_PLATFORMS} -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/ --push
+	perl -i -pe's|.*FROM '"${registry}"'|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
+	docker buildx rm mybuilder
 else
 	registry='Dakuchi/'
-	docker  build -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/ --load
-	docker  build -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/ --load
-	docker  build -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/ --load
-	docker  build -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/ --load
-	docker  build -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/ --load
-	docker  build -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/ --load
-	docker  build -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/ --load
-	docker  build -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/ --load
-	docker  build -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/ --load
+	docker buildx build -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/ --load
+	docker buildx build -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/ --load
+	docker buildx build -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/ --load
+	docker buildx build -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/ --load
+	docker buildx build -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/ --load
+	docker buildx build -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/ --load
+	docker buildx build -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/ --load
+	docker buildx build -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/ --load
+	docker buildx build -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/ --load
 fi
 
